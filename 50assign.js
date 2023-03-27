8a9d473d60186625335c9245d823b4540c67b35d
ramyakondi12345 <125718223+ramyakondi12345@users.noreply.github.com>
2023-03-27 21:30:39 +0530

    Add files via upload

---
+++ 50assign.js
@@ -0,0 +1,10 @@
+let y=50;
+console.log("the original value of y is",y);
+
+console.log("the value of y at preincrement",++y);
+console.log("the value of y at postincrement",y++);
+console.log("the value of y after post increment",y);
+
+console.log("the value of y at predecrement",--y);
+console.log("the value of y at postincrement",y--);
+console.log("the value of y after post increment",y);
